fb06dcdc5e4f2ab92d7a3ed6b49fed44184e8e95
sherifHaraedy <sherif.m.haraedy@gmail.com>
2025-04-18 21:59:30 +0200

    Corrected Typo.

--- src/map.py
+++ src/map.py
@@ -3,5 +3,5 @@ import numpy as np
 from config import HEX_W, HEX_H
 
 class Map:
-    def __init__(self, width = HEX_W, heigt = HEX_H):
+    def __init__(self, width = HEX_W, height = HEX_H):
         pass
