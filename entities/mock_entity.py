1651d9154a4faa6fcaa4aedad3b8018119ffb70f
mesterbanana <159573166+mesterbanana@users.noreply.github.com>
2025-04-24 17:29:21 +0200

    Delete entities directory

--- entities/mock_entity.py
+++
@@ -1,9 +0,0 @@
-from .. import Game, pygame
-from config import ENTITY_COLOR
-
-class Entity(pygame.sprite.Sprite, Game):
-    def __init__(self, position, radius, color=ENTITY_COLOR, width=0):
-        pygame.sprite.Sprite.__init__(self)
-        screen = Game().screen
-        self.rect = pygame.draw.circle(screen, position, color, radius, width)
-        self.area = screen.get_rect()
