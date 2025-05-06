e7a82f2d97ff50621a847f1813bd36018aedc0f2
sherifHaraedy <sherif.m.haraedy@gmail.com>
2025-05-06 14:13:46 +0300

    DEBUG is set by env.

--- config.py
+++ config.py
@@ -4,9 +4,6 @@ from dotenv import load_dotenv
 WIDTH, HEIGHT = 800, 600
 FPS = 60
 
-HEX_W = 30
-HEX_H = 30
-
 HEX_COLOR = (30, 30, 30)
 
 ENTITY_COLOR = (46, 204, 113)
@@ -14,4 +11,4 @@ ENTITY_COLOR = (46, 204, 113)
 OFFSET = (WIDTH/2, HEIGHT/2)
 
 load_dotenv()
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = os.getenv("DEBUG")
