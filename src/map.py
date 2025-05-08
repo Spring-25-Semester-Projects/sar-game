abe68fdfce9afaaef7b53b6f7b40206324d29c3c
Mimo <omar1512khalifa@gmail.com>
2025-05-08 12:36:55 +0300

    map.py

--- src/map.py
+++ src/map.py
@@ -1,25 +1,17 @@
+import pygame
 import numpy as np
+from config import HEX_W, HEX_H
+from hex import Hex, Center
 from config import WIDTH, HEIGHT, HEX_COLOR
 
-CONST_unit_direction = np.array([[1, -1, 0],[1, 0, -1],[0, 1, -1],[-1, 1, 0],[-1, 0, 1],[0, -1, 1]])
+class Map:
+    def __init__(self, width = HEX_W, height = HEX_H):
+        pass
+CONST_unit_direction = np.array([[0, -1, 1], [1, -1, 0], [-1, 0, 1], [0, 1, -1], [1, 0, -1], [-1, 1, 0]])
 
 CONST_flatTopped_matrix = np.array([[3/2, 0],[np.sqrt(3)/2, np.sqrt(3)]])
 
 CONST_screen_matrix = np.array([[0, 0],[WIDTH, 0],[0, HEIGHT],[WIDTH, HEIGHT]]) - np.array([WIDTH/2,HEIGHT/2]) # Sceond term is to center.
-
-class Center:
-    def __init__(self, q=0, r=0):
-        self.q, self.r = q, r
-
-class Hex:
-    def __init__(self, x=0, y=0, z=0):
-        self.x, self.y, self.z = x, y, z
-
-    def __hash__(self):
-        hq = hash(self.x)
-        hr = hash(self.y)
-
-        return hq ^ (hr + 0x9e3779b9 + ((hq << 6) & 0xFFFFFFFFFFFFFFFF) + (hq >> 2))
 
 class Map(Hex):
     def __init__(self, radius=10):
@@ -45,27 +37,27 @@ class Map(Hex):
         hexagon_matrix = self.radius * (CONST_flatTopped_matrix @ np.array([hex.x, hex.z]))
 
         return Center(*hexagon_matrix)
-    
-    def screen_to_hex(self, hex: Hex):
-        center = self.hex_to_screen(hex)
 
-        radius = np.sqrt(3)/2
-        t1, t2 = center.q, center.r/radius
+    def screen_to_hex(self, point):
+        scale = np.sqrt(3)/2
 
-        z = np.floor((np.floor(center.r / radius) + np.floor(t2 - t1) + 2) / 3)
-        x = np.floor((np.floor(t1 - t2) + np.floor(t2 - t1) + 2) / 3)
+        t1 = point[0] / self.radius
+        t2 = (point[1] / self.radius) / np.sqrt(3)
+
+        y = np.floor((np.floor((point[1] / self.radius) / scale) + np.floor(t2 - t1) + 2.0) / 3.0)
+        x = np.floor((np.floor(t1 - t2) + np.floor(t1 + t2) + 2.0) / 3.0)
 
         class Cube:
             def __init__(self, a=0, b=0, c=0):
-                self.a, self.b, self.c = a, b, c
+                self.a, self.b, self.c = int(a), int(b), int(c)
 
-        return Cube(x,-x-z,z)
+        return Cube(x,y,-x-y)
     
     def neighbor_hex(self, hex: Hex):
         neighbors_matrix = (np.array([hex.x, hex.y, hex.z]) + CONST_unit_direction)
 
         return neighbors_matrix
-    
+
     def draw_hex(self, hex: Hex, entityHex=None):
         center = self.hex_to_screen(hex)
 
@@ -90,3 +82,17 @@ class Map(Hex):
             vertices.append([x + WIDTH/2,y + HEIGHT/2])
 
         return vertices, color
+
+    def initialize_fog_of_war(self):
+        self.fog_of_war = {hex_tile: False for hex_tile in self.hexes.values()}  
+
+    def update_visibility(self, center_hex):
+       
+        self.fog_of_war[center_hex] = True
+        
+        for direction in CONST_unit_direction:
+            neighbor = Hex(center_hex.x + direction[0], 
+                        center_hex.y + direction[1], 
+                        center_hex.z + direction[2])
+            if neighbor in self.fog_of_war:
+                self.fog_of_war[neighbor] = True
