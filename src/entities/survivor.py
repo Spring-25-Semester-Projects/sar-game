5b2d6e6e98b4f305bd1f7f2e427510901b6eface
sherifHaraedy <sherif.m.haraedy@gmail.com>
2025-05-12 03:42:08 +0300

    Integrated,added some survivor functions.

--- src/entities/survivor.py
+++ src/entities/survivor.py
@@ -1,3 +1,34 @@
-class Survivor:
-    def __init__(self):
-        pass
+import numpy as np
+from entities.mock_entity import *
+
+class Survivor(Entity):
+    def __init__(self, map, color=ENTITY_COLOR):
+        super().__init__(map, color)
+        self.stamina = 100
+        self.hunger = 0
+        self.stats = [self.points, self.stamina, self.hunger]
+        self.sane = True # Well, for now.
+
+        def hurt(self, vl):
+            self.stats -= vl
+
+            return self.stats
+        
+        def eat(self, vl):
+            hurt([0, max(self.hunger - vl, 0), min(self.stamina + ((1/3) * vl), 100)])
+        
+        def decide(self, need_to_move):
+            if not self.sane:
+                super().move()
+            else:
+                return CONST_direction[np.random.randint(0,6)]
+
+
+        
+
+            
+            
+            
+        
+            
+
