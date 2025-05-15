22900e84d3da4da6ebc73aa8004d1f20fbc9b66b
sherifHaraedy <sherif.m.haraedy@gmail.com>
2025-05-15 20:09:32 +0300

    Basic Rescuer format.

--- src/entities/rescuer.py
+++ src/entities/rescuer.py
@@ -1,3 +1,13 @@
-class Rescuer:
-    def __init__(self):
-        pass
+from entities.mock_entity import *
+
+class Rescuer(Entity):
+    def __init__(self, map):
+        super().__init__(map)
+        self.resources = 100
+        self.stats = [self.points, self.resources]
+    
+    def replinsh(self, vl):
+        self.resources += max(self.hunger - vl, 0)
+    
+    def seek(self):
+        return None # Path-finder later.
