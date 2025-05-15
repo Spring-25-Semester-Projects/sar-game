66ed044badd8cb27b0838d09415a548a1ff16495
sherifHaraedy <sherif.m.haraedy@gmail.com>
2025-05-15 20:09:40 +0300

    Basic Survivor format.

--- src/entities/survivor.py
+++ src/entities/survivor.py
@@ -2,26 +2,27 @@ import numpy as np
 from entities.mock_entity import *
 
 class Survivor(Entity):
-    def __init__(self, map, color=ENTITY_COLOR):
-        super().__init__(map, color)
+    def __init__(self, map):
+        super().__init__(map)
+
+        # ! THIS IS ONLY FOR TESTING, DEFAUT SPAWN POSITION IS BASED ON SEED
+        self.hexEntity = Hex(-12,6,6)
+        self.position = self.entity_position()
+
         self.stamina = 100
         self.hunger = 0
         self.stats = [self.points, self.stamina, self.hunger]
         self.sane = True # Well, for now.
-
-        def hurt(self, vl):
-            self.stats -= vl
-
-            return self.stats
         
         def eat(self, vl):
-            hurt([0, max(self.hunger - vl, 0), min(self.stamina + ((1/3) * vl), 100)])
+            self.stats += ([0, max(self.hunger - vl, 0), min(self.stamina + ((1/3) * vl), 100)])
         
-        def decide(self, need_to_move):
+        def decide(self):
             if not self.sane:
-                super().move()
+                return super().move(CONST_direction[np.random.randint(0,6)])
+            
             else:
-                return CONST_direction[np.random.randint(0,6)]
+                return None # Minimax Function later.
 
 
         
