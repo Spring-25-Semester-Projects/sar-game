d1a661819aae385081f0b9ae04d7a3a16429d868
Mimo <omar1512khalifa@gmail.com>
2025-05-18 11:59:59 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,19 @@
 import os
+import numpy as np
 from dotenv import load_dotenv
 
-WIDTH, HEIGHT = 800, 600
+WIDTH, HEIGHT = 1400, 800
 FPS = 60
 
-HEX_W = 30
-HEX_H = 30
+HEX_COLOR = (50, 50, 50)
 
-HEX_COLOR = (30, 30, 30)
+ENTITY_COLOR = (46, 204, 113)
 
-ENTITY_COLOR = (0,0,0)
+COST_COLORS = [(220, 20, 60), (178, 34, 34), (205, 92, 92), (255, 99, 71), (139, 0, 0), (240, 128, 128), (93, 58, 102)]
+
+OFFSET = np.array([WIDTH/2, HEIGHT/2])
+
+SIZE = 38
 
 load_dotenv()
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = os.getenv("DEBUG")
