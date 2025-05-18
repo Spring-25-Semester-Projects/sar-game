f47652c0b1a169e9cc3ee53b54454161a45ca4b0
Mimo <omar1512khalifa@gmail.com>
2025-05-18 11:59:19 +0300

    Update map.py

--- src/map.py
+++ src/map.py
@@ -1,20 +1,21 @@
-import pygame
+from collections import namedtuple
 import numpy as np
-from config import HEX_W, HEX_H
-from hex import Hex, Center
-from config import WIDTH, HEIGHT, HEX_COLOR
+from src.hex import Hex, Center
+from config import WIDTH, HEIGHT, HEX_COLOR, SIZE, OFFSET
 
-class Map:
-    def __init__(self, width = HEX_W, height = HEX_H):
-        pass
 CONST_unit_direction = np.array([[0, -1, 1], [1, -1, 0], [-1, 0, 1], [0, 1, -1], [1, 0, -1], [-1, 1, 0]])
+
+CONST_scale_matrix = np.array([[1,0],[0,(1.15)*(3/2)/(np.sqrt(3))]])
 
 CONST_flatTopped_matrix = np.array([[3/2, 0],[np.sqrt(3)/2, np.sqrt(3)]])
 
-CONST_screen_matrix = np.array([[0, 0],[WIDTH, 0],[0, HEIGHT],[WIDTH, HEIGHT]]) - np.array([WIDTH/2,HEIGHT/2]) # Sceond term is to center.
+CONST_screen_matrix = np.array([[0, 0],[WIDTH, 0],[0, HEIGHT],[WIDTH, HEIGHT]]) - OFFSET # Sceond term is to center.
 
-class Map(Hex):
-    def __init__(self, radius=10):
+Costs = namedtuple('Cost', ['zero', 'one', 'two', 'three', 'four', 'block'])
+CONST_cost = tuple(Costs(zero=0, one=1, two=2, three=3, four=4, block=float('-inf')))
+
+class Map:
+    def __init__(self, radius=SIZE):
         self.radius = radius
 
         # This long variable just turns the pixel coord.’s to hex coord. (for screen), so it knows where the tile boundary is.
@@ -22,56 +23,105 @@ class Map(Hex):
 
         min_screenHex = np.floor(np.min(screen_to_hex_matrix, axis=0))-2
         max_screenHex = np.ceil(np.max(screen_to_hex_matrix, axis=0))+2
+
+        self.min_x, self.min_z = min_screenHex
+        self.max_x, self.max_z = max_screenHex
+
         min_x, min_z = map(int, min_screenHex)
         max_x, max_z = map(int, max_screenHex)
 
         self.hexes = {}
         for q in range(min_x, max_x + 1):
             for r in range(min_z, max_z + 1):
-                s = -q-r
-                h = Hex(q, r, s)
+                px, py = (self.radius * (CONST_scale_matrix @ CONST_flatTopped_matrix @ np.array([q, r]))) + OFFSET
 
-                self.hexes[h] = h
+                if 0 <= px < WIDTH and 0 <= py < HEIGHT:
+                   s = -q-r
+                   h = Hex(q, r, s)
+
+                   self.hexes[h] = h
 
     def hex_to_screen(self, hex: Hex):
         hexagon_matrix = self.radius * (CONST_flatTopped_matrix @ np.array([hex.x, hex.z]))
 
         return Center(*hexagon_matrix)
-
+    
     def screen_to_hex(self, point):
         scale = np.sqrt(3)/2
 
         t1 = point[0] / self.radius
         t2 = (point[1] / self.radius) / np.sqrt(3)
 
-        y = np.floor((np.floor((point[1] / self.radius) / scale) + np.floor(t2 - t1) + 2.0) / 3.0)
+        z = np.floor((np.floor((point[1] / self.radius) / scale) + np.floor(t2 - t1) + 2.0) / 3.0)
         x = np.floor((np.floor(t1 - t2) + np.floor(t1 + t2) + 2.0) / 3.0)
 
-        class Cube:
-            def __init__(self, a=0, b=0, c=0):
-                self.a, self.b, self.c = int(a), int(b), int(c)
+        return self.hexes.get(Hex(int(x),int(-x-z),int(z)))
 
-        return Cube(x,y,-x-y)
-    
     def neighbor_hex(self, hex: Hex):
-        neighbors_matrix = (np.array([hex.x, hex.y, hex.z]) + CONST_unit_direction)
+        neighbors_matrix = (np.array([*hex]) + CONST_unit_direction)
 
-        return neighbors_matrix
+        neighbors = list()
 
-    def draw_hex(self, hex: Hex, entityHex=None):
+        for nb in neighbors_matrix:
+            if not self.hexes.get(Hex(*nb)):
+                neighbors.append(None)
+            else:
+                neighbors.append(self.hexes[Hex(*nb)])
+
+        return np.array(neighbors, dtype=object)
+    
+    def hex_distance(self, h1 : Hex, h2 : Hex):
+        return max(abs(h1.x - h2.x), abs(h1.y - h2.y), abs(h1.z - h2.z))
+    
+    def hex_round(self, hex: Hex):
+        fcube = np.array([*hex])
+        cube = np.round(fcube)
+
+        diff = np.abs(cube-fcube)
+
+        max_i = np.argmax(diff)
+
+        cube[max_i] = -np.sum(np.delete(cube, max_i))
+
+        hex = Hex(*np.int32(cube))
+        
+        return hex
+
+    def walkable_hex_distance(self, h1 : Hex, h2 : Hex):
+        distance = self.hex_distance(h1,h2)
+
+        if distance == 0:
+            return [h1]
+
+        interpol_vl = np.linspace(1, distance, num=distance)
+
+        hexes_to_walk = []
+        def interpol():
+            cube_h1 = np.array([*h1])
+            cube_h2 = np.array([*h2])
+
+            for t in interpol_vl:
+                i = cube_h1 + ((cube_h2 - cube_h1) * t * (1/distance))
+
+                hexes_to_walk.append(self.hex_round(Hex(*i)))
+
+            return hexes_to_walk
+
+        return interpol()
+    
+    def map_cost(self):
+        costs = {}
+        
+        for h in self.hexes.values():
+            costs[h] = CONST_cost[np.random.randint(0,6)]
+
+        return costs
+
+    def draw_hex(self, hex: Hex):
         center = self.hex_to_screen(hex)
 
         vertices = []
         color = HEX_COLOR
-
-        if entityHex is not None:
-            neighbors = self.neighbor_hex(entityHex)
-
-            for hex_nb in neighbors:
-                if np.array_equal([hex.x, hex.y, hex.z], hex_nb):
-                    color = (231, 76, 60)
-
-                    break
 
         for i in range(6):
             angle = np.radians(60 * i)
@@ -79,20 +129,6 @@ class Map(Hex):
             x = center.q + (self.radius * np.cos(angle))
             y = center.r + (self.radius * np.sin(angle))
 
-            vertices.append([x + WIDTH/2,y + HEIGHT/2])
+            vertices.append(np.array([x,y])+OFFSET)
 
         return vertices, color
-
-    def initialize_fog_of_war(self):
-        self.fog_of_war = {hex_tile: False for hex_tile in self.hexes.values()}  
-
-    def update_visibility(self, center_hex):
-       
-        self.fog_of_war[center_hex] = True
-        
-        for direction in CONST_unit_direction:
-            neighbor = Hex(center_hex.x + direction[0], 
-                        center_hex.y + direction[1], 
-                        center_hex.z + direction[2])
-            if neighbor in self.fog_of_war:
-                self.fog_of_war[neighbor] = True
